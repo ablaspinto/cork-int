22255b55ce13cad9e209ffbabd9663e00833711b
ablaspinto <ablaspinto@gmail.com>
2025-10-20 23:25:39 -0400

    update main file to check if input proper csvv and epss apis are working

--- src/my_script.py
+++ src/my_script.py
@@ -1,5 +1,6 @@
 import argparse
 from components.catalog import download_kev , find_kev
+from components.fetch_api_request import fetch_cve, fetch_epss
 import os
 
 
@@ -9,7 +10,7 @@ def construct_risk_objects(epss_map, cve_scores, kev_scores):
 
 
 def main():
-    if not os.path.exists("./catalog/kev.json"):
+    if not os.path.exists("./kev_store/kev.json"):
         download_kev()
     argument_parser = argparse.ArgumentParser(description="CLI Tool that calculates risk based on CVSS Score, EPSS, and KEV")
     argument_parser.add_argument("vendor" , type=str, help="enter a name of a vendor, for example microsoft, adobe")
@@ -19,13 +20,11 @@ def main():
     vendor = arguments.vendor.lower()
     prod = arguments.product.lower()
     print("Arguments put in:", "Vendor:",vendor ,"Product:", prod)
-    
-    
 
-    
-
-
-
+    cve_list = fetch_cve(vendor,prod)
+    print(cve_list)
+    epss_list = fetch_epss(cve_list)
+    print(epss_list)
 
 if __name__ == "__main__":
     main()

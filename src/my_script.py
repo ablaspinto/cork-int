03c9a1c5f1ca5ab4625a71d77ebd57f66f039e1b
ablaspinto <ablaspinto@gmail.com>
2025-10-22 23:08:37 -0400

    updated main, to properly work, completing readme now

--- src/my_script.py
+++ src/my_script.py
@@ -1,13 +1,42 @@
 import argparse
 from components.catalog import download_kev , find_kev
 from components.fetch_api_request import fetch_cve
+from components.risk_object import RiskObject
 import os
 
 
 def construct_risk_objects(cve_list, kev_list):
-    ...
-    
+    kev_status = False
+    list_objects = list()
+    if len(kev_list) > 0:
+        for i in range(len(kev_list)):
+            kev_map = kev_list[i]
+            for j in range((len(cve_list))):
+                id = cve_list[i].get("cve_id")
+                desc= cve_list[i].get("desc")
+                cvss = cve_list[i].get("cvss")
+                epss = cve_list[i].get("epss")
+                if kev_list[i].get("cve_id") == id:
+                    risk_obj = RiskObject(id,desc,cvss,epss,0,True,0)
+                    list_objects.append(risk_obj)
+                else:
+                    risk_obj = RiskObject(id,desc,cvss,epss)
+                    list_objects.append(risk_obj)
+    else:
+        for i in range(len(cve_list)):
+            id = cve_list[i].get("cve_id")
+            desc= cve_list[i].get("desc")
+            cvss = cve_list[i].get("cvss")
+            epss = cve_list[i].get("epss")
+            risk_obj = RiskObject(id,desc,cvss,epss)
+            risk_obj.assess_risk()
+            list_objects.append(risk_obj)
+    return list_objects
 
+
+def represent_risk_obj(list_obj):
+    for i in range(len(list_obj)):
+        print(list_obj[i])
 
 
 def main():
@@ -26,6 +55,8 @@ def main():
     print("Arguments put in:", "Vendor:",vendor ,"Product:", prod)
     cve_list = fetch_cve(vendor,prod,app_type,skip_amount)
     kev_list = find_kev(cve_list)
+    list_object = construct_risk_objects(cve_list,kev_list)
+    represent_risk_obj(list_object)
 
 
 if __name__ == "__main__":
